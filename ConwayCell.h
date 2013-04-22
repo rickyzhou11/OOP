5854c7a50ffaa60ed805b5248b9a9c167015edbb
Chuyu Zhou <rickyzhou@utexas.edu>
2013-04-21 22:02:09 -0500

    added conwaycell

---
+++ ConwayCell.h
@@ -0,0 +1,93 @@
+#ifndef ConwayCell_h
+#define ConwayCell_h
+
+#include<iostream> 
+
+
+Class ConwayCell  : public AbstractCell {
+	
+	public:
+		ConwayCell (bool alive) : 
+			AbstractCell (alive),
+			_num_neighbors (0),
+			_age (0),
+			
+
+			{}
+
+	void ConwayCell::update_status() {
+		}
+
+
+	void ConwayCell::set_num_neighbors(int row, int col){ 
+
+		Cell* ptr;
+		//check the life status of the cell above it
+		if ( (row-1)>= 0){
+			ptr = &life_board[row-1][col];
+			if(ptr->_alive)
+				++_num_neighbors; 
+		}
+		//check the life status of the cell to the left of current cell
+		if( (col-1)>= 0){
+			ptr = &life_board[row][col-1];
+			if(ptr-> _alive)
+				++_num_neighbors;
+		}
+		//check upper left diagonal
+		if( (row-1)>=0 && (col-1)>=0){
+			ptr = &life_board[row-1][col-1]; 
+			if(ptr->_alive)
+				++_num_neighbors;
+		}
+
+		//check below current cell
+		if( (row+1)< board_row){
+			ptr = &life_board[row+1][col]; 
+			if(ptr-> _alive)
+				++_num_neighbors;		
+		}
+
+		//check cell to the right of current cell
+		if( (col+1) < board_col){
+			ptr = &life_board[row][col+1];
+			if(ptr->_alive)
+				++_num_neighbors;
+		}
+		//check cell bottom left diagonal
+		if( (row+1) < board_row && (col-1)>=0){
+			ptr = &life_board[row+1][col-1];
+			if(ptr->_alive)
+				++_num_neighbors; 
+		}
+
+		//check upper right diagonal
+		if( (row-1)>=0 && (col+1)<board_col ){
+			ptr = &life_board[row-1][col+1]
+			if(ptr->_alive)
+				++_num_neighbors;
+
+		}
+		//check bottom right diagonal
+		if( (row+1)<board_row && (col+1)<board_col ){
+			ptr = &life_board[row+1][col+1];
+			if(ptr->_alive)
+				++_num_neighbors;
+		}	
+
+	}
+
+	void ConwayCell::update_status(){
+		
+		if( !_alive && (_num_neighbors==3) )
+			_alive = true;
+
+		if( alive){
+			if(_num_neighbors < 2 || _num_neighbors > 3 )
+				_alive= false; 
+			
+		}
+	}
+		
+}
+	
